91c5381357a27b62861b1748c906a0b430dfcc62
MRGRD56 <Bukinist56@mail.ru>
2021-07-24 12:25:38 +0500

    first commit

---
+++ HumanReadableDurationFormat/HumanTimeFormat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HumanReadableDurationFormat
+{
+    public static class HumanTimeFormat
+    {
+        public static string formatDuration(int seconds)
+        {
+            if (seconds == 0)
+            {
+                return "now";
+            }
+
+            if (seconds < 0)
+            {
+                throw new ArgumentException("Seconds value must be non-negative", nameof(seconds));
+            }
+
+            var time = TimeSpan.FromSeconds(seconds);
+            var timeComponents = new Dictionary<string, int>
+            {
+                {"year", time.Days / 365},
+                {"day", time.Days % 365},
+                {"hour", time.Hours},
+                {"minute", time.Minutes},
+                {"second", time.Seconds}
+            };
+
+            var stringTimeComponents = new List<string>();
+
+            foreach (var (unit, value) in timeComponents)
+            {
+                if (value == 0) continue;
+                stringTimeComponents.Add($"{value} {unit + (value == 1 ? "" : "s")}");
+            }
+
+            var resultLeftPart = string.Join(", ", stringTimeComponents.Take(stringTimeComponents.Count - 1));
+            
+            var result = resultLeftPart + 
+                         (string.IsNullOrEmpty(resultLeftPart) ? "" : " and ") +
+                         stringTimeComponents.Last();
+
+            return result;
+        }
+    }
+}
---
+++ HumanReadableDurationFormat/Program.cs
@@ -0,0 +1,13 @@
+﻿//https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/csharp
+using System;
+
+namespace HumanReadableDurationFormat
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(HumanTimeFormat.formatDuration(132030240));
+        }
+    }
+}
