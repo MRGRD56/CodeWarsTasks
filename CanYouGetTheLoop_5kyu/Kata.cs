777bdc2bedf4c66a312e5e6230f97934c55b6a23
MRGRD56 <Bukinist56@mail.ru>
2021-07-28 22:23:39 +0500

    fail https://www.codewars.com/kata/52a89c2ea8ddc5547a000863/train/csharp

---
+++ CanYouGetTheLoop_5kyu/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CanYouGetTheLoop_5kyu
+{
+    public static class Kata
+    {
+        public static int GetLoopSize(LinkedListNode<string> startNode)
+        {
+            var previousNodes = new List<LinkedListNode<string>>();
+            var currentNode = startNode;
+            while (true)
+            {
+                var nextNode = currentNode?.Next;
+                if (nextNode == null) break;
+
+                if (previousNodes.Contains(nextNode))
+                {
+                    return previousNodes.Count - previousNodes.IndexOf(nextNode);
+                }
+                
+                previousNodes.Add(nextNode);
+                currentNode = nextNode;
+            }
+
+            return 0;
+        }
+    }
+}
